8a59835fdd74ae361c60ba8423ce254af6bebfdc
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-02-04 09:56:35 +0100

    quick

--- _src/main.cpp
+++ _src/main.cpp
@@ -48,6 +48,7 @@ struct single_question
 {
     u16 status; // 0 - not answered // 1 - answered
 
+    string group_name;
     string question;
     string answer_a;
     string answer_b;
@@ -56,19 +57,21 @@ struct single_question
     string answer_correct;
     string explanation;
 
-    single_question(string _question, string _answer_a, string _answer_b,
-                    string _answer_c, string _answer_d, string _answer_correct,
-                    string _explanation)
-        : status(0), question(_question), answer_a(_answer_a),
-          answer_b(_answer_b), answer_c(_answer_c), answer_d(_answer_d),
-          answer_correct(_answer_correct), explanation(_explanation)
+    single_question(string _group_name, string _question, string _answer_a,
+                    string _answer_b, string _answer_c, string _answer_d,
+                    string _answer_correct, string _explanation)
+        : status(0), group_name(_group_name), question(_question),
+          answer_a(_answer_a), answer_b(_answer_b), answer_c(_answer_c),
+          answer_d(_answer_d), answer_correct(_answer_correct),
+          explanation(_explanation)
     {
     }
 };
 
 struct Format_Buffer
 {
-    static single_question input_log_line_output_obj(const string &log_line)
+    static single_question input_log_line_output_obj(const string &log_line,
+                                                     const string &group_name)
     {
         auto result = UTILS::str::split_string(log_line, ';');
         cout << endl << endl;
@@ -79,8 +82,8 @@ struct Format_Buffer
                 r.erase(0, 1);
         }
 
-        return {result[0], result[1], result[2], result[3],
-                result[4], result[5], result[6]};
+        return {group_name, result[0], result[1], result[2],
+                result[3],  result[4], result[5], result[6]};
     }
 };
 
@@ -197,6 +200,8 @@ class Test
 
     single_question &random_question()
     {
+        static string previous_group = "";
+
         u64 random_index{};
         do
         {
@@ -238,42 +243,39 @@ class Test
     {
         auto all_lines = File_Utils::read_from(input_file);
 
+        string group_name;
         for (auto &line : all_lines)
         {
             if (line != "\n" && line != "\r\n" && line != "")
             {
-                if (line)
+                if (line.find("[#") == std::string::npos)
+                {
+                    group_name = line;
+                    continue;
+                }
 
-                    if (str.find(substring) == std::string::npos)
-                    {
-                        std::cout
-                            << "The string does NOT contain the substring."
-                            << std::endl;
-                    }
-                    else
-                    {
-                        std::cout << "The string contains the substring."
-                                  << std::endl;
-                    }
+                auto question =
+                    Format_Buffer::input_log_line_output_obj(line, group_name);
 
-                // auto question =
-                // Format_Buffer::input_log_line_output_obj(line);
+                cout << question.group_name << endl;
 
-                // cout << question.question << endl;
-                // cout << question.answer_a << endl;
-                // cout << question.answer_b << endl;
-                // cout << question.answer_c << endl;
-                // cout << question.answer_d << endl;
-                // cout << question.answer_correct << endl;
-                // cout << question.explanation << endl;
-
-                // all_questions.push_back(question);
+                all_questions.push_back(question);
             }
         }
 
-        return;
+        // for(auto& question : all_questions)
+        // {
+        //     cout << "->" << question.group_name << "<-" << endl;
+        //     cout << question.question << endl;
+        //     cout << question.answer_a << endl;
+        //     cout << question.answer_b << endl;
+        //     cout << question.answer_c << endl;
+        //     cout << question.answer_d << endl;
+        //     cout << question.answer_correct << endl;
+        //     cout << question.explanation << endl;
+        // }
 
-        start();
+        // start();
     }
 };
 
